6c5e4c1f9ee1532e04486f0ca45393355659baf4
Juan Jesús Martin <juanjesus.martin@viewnext.com>
2023-09-01 13:46:18 +0200

    se instala @nestjs/typeorm typeormy el driver pg que es de postgres con su respectiva configuracion en app.module y creacion de variables de entorno en .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,  //para que carge las entidades automaticamente
+      synchronize:true, //cuando de realizan cambios en las entidades inmediatamente se sincroniza EN PRO = false
+    })
   ],
 })
 export class AppModule {}
