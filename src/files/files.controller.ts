e25be133abae14e8ab65c615e3c21d875f58fd18
Juan Jesús Martin <juanjesus.martin@viewnext.com>
2023-09-06 19:17:58 +0200

    se asigna el endpoint para servir la imagen

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter,fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Param('imageName') imageName:string,
+    @Res() res: Response
+  ){
+   const path= this.filesService.getStaticProductImage(imageName);
+    
+   res.sendFile(path)
+  
+  return path;
+  }
+
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file',{
