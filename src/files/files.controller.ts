5082af9ef9f2ee59c27a6122a483930b571c17bc
Juan Jesús Martin <juanjesus.martin@viewnext.com>
2023-09-06 14:52:42 +0200

    usar un interceptor con el nombre esperado del archivo y filtrarlo

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,19 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file',{fileFilter:fileFilter}))
   uploadProductImage(
     @UploadedFile() file: Express.Multer.File,
     
     ){
-
-      console.log(file);
+      if(!file){
+        throw new BadRequestException('Make sure that file is a image');
+      }
 
     return {
       fileName: file.originalname
