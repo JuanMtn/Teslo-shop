4cdee01f1b2ec625a8b3efe3308fab44bae251e5
Juan Jesús Martin <juanjesus.martin@viewnext.com>
2023-09-08 16:10:43 +0200

    se crea la funcion login en el service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from "bcrypt";
+
+
 @Injectable()
 export class AuthService {
 
@@ -31,6 +33,26 @@ export class AuthService {
       this.handleDBErrors(error);
     }
 
+  }
+
+  async login(loginUserDto: LoginUserDto){
+
+    const {password, email}= loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select:{email:true,password:true}
+    })
+
+    if(!user)
+    throw new UnauthorizedException(`Credencials are not valid (email)`);
+  
+    if(!bcrypt.compareSync(password,user.password))
+      throw new UnauthorizedException(`Credencials are not valid (password)`);
+
+      //TODO return JWT
+      
+    return user;
   }
 
   private handleDBErrors(error:any):never{
